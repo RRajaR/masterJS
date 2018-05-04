465ee94f206f8a7f61f87e7b3f2a487072cc6a9b
Raja <ratnam.raja.r@gmail.com>
2018-05-04 09:41:34 +0530

    Create Inheritance.js

---
+++ OOP-Js/Inheritance.js
@@ -0,0 +1,18 @@
+function Car(type,color,engine)
+{
+this.type = type;
+this.color = color;
+this.engine = engine;
+};
+
+Car.prototype.carEngine = function(){
+console.log("this car is having " + this.engine);
+}
+
+
+function Benz(type,color,engine,speed)
+{
+Car.call(this,type,color,engine);
+this.speed = speed;
+}
+
