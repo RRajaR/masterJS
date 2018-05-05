e6d041870ea8b585d95106705f9000a4fdc2d944
Raja <ratnam.raja.r@gmail.com>
2018-05-06 02:31:04 +0530

    Update InheritanceClass.js

--- OOP-Js/InheritanceClass.js
+++ OOP-Js/InheritanceClass.js
@@ -1,31 +1,32 @@
-class Car
-{
-construcor(type,color,engine){
+class Car{
+constructor(type,color,engine){
 this.type = type;
-this.color = color;
-this.engine = engine;
+  this.color = color;
+  this.engine = engine
+}
+  
+  makeEngine()
+  {
+  console.log("this is having "+this.engine);
+  }
 }
 
-carEngine()
+class Audi extends Car
 {
-console.log("this car is having " + this.engine);
-}
-}
-
-
-class Benz extends Car
-{
-constructor(type,color,engine,speed)
-{
-super(type,color,engine);
-this.speed = speed;
-}
-
-carEngine()
-{
-return this.engine;
-}
+  constructor(type, color, engine, speed)
+  {
+    super(type, color, engine);
+    this.speed = speed;
+  }
+ 
+   makeEngine()
+  {
+  console.log(this.engine);
+  }
 }
 
-let carOne = new Car("model s3","Lightgrey","Auto2400");
-carOne.carEngine();
+let carOne = new Car("Range", "gold", "Auto2400", "500");
+carOne.makeEngine();  // this is having Auto2400
+
+let carTwo = new Audi("Range", "gold", "Auto2400");
+carTwo.makeEngine();  // Auto2400
