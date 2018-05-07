45dbcb81d7131eed4666b0ab2555a48a5503cd69
Raja <ratnam.raja.r@gmail.com>
2018-05-07 10:22:26 +0530

    Update abstraction.js

--- OOP-Js/abstraction.js
+++ OOP-Js/abstraction.js
@@ -2,27 +2,21 @@ class Foo {
     constructor(text){
        this.text = text;
     }
- 
     fooFunction() {
         console.log("This is super class. "+this.text);
     }
- 
 }
  
 class Bar extends Foo {
- 
     constructor(text){
        super(text);
     }
- 
     fooFunction() {
         console.log('This is child class. '+this.text);
     }
-    
     barFunction() {
        console.log('Method implemented successfully.');
-    }
-    
+    } 
 }
  
 let b = new Bar('Hey!');
