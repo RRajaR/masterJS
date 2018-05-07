36bafd111ab25c60d18159e2a1213e6f44a07776
Raja <ratnam.raja.r@gmail.com>
2018-05-07 17:09:47 +0530

    Update Encapsulation.js

--- OOP-Js/Encapsulation.js
+++ OOP-Js/Encapsulation.js
@@ -1,17 +1,9 @@
 function Person()
 {
-  let name = "Megan";
   let height = 68;
-  let weight = 170;
 
   this.setHeight = function(newHeight) {height=newHeight;}
   this.getHeight = function() { return height; }
-  this.setWeight = function(newWeight) {weight = newWeight;}
-  this.getWeight = function() { return weight; }
-  this.setName   = function(newName) {name=newName;}
-  this.getName   = function() { return name; }
-
-  return this;
 }
 
 var a = new Person();
