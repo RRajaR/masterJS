cd744cc459a2dcb0a40e60b07bedefe47f24c46d
Raja <ratnam.raja.r@gmail.com>
2018-05-09 12:38:26 +0530

    Update debounce.js

--- Js Advanced/debounce.js
+++ Js Advanced/debounce.js
@@ -1,18 +1,27 @@
-function debounce(func, wait)
-{
-let timeout;
-return function(...args){
-    const context = this;
-    clearTimeout(timeout);
-    timeout = setTimeout(()=>func.apply(context,args),wait);
-}
-}
+const debounce = (fn, time) => {
+    let timeout;
+  
+    return function (...args) { // <-- not an arrow function
+      const functionCall = () => fn.apply(this, args);
+  
+      clearTimeout(timeout);
+      timeout = setTimeout(functionCall, time);
+    }
+  }
+  
+  const obj = {
+    name: 'foo',
+    sayMyName() {
+      console.log('My name is', this.name);
+     this.sayMyName = debounce(this.sayMyName,2000);
+      
+    }
+  }
+  
+  obj.sayMyName(); 
+  obj.sayMyName = debounce(obj.sayMyName, 1000);
+  obj.sayMyName = debounce(obj.sayMyName, 1000);
+  obj.sayMyName(); 
 
-function sayHi()
-{
-console.log("my name is "+ this.name);
-}
 
-const megan = {name : "Megan", callfun : debounce(sayHi)}
-
-megan.callfun();
+ 
